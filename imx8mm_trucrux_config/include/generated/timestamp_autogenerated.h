b7846b66ed21ededa862848892405ba1e6387f58
saahilshinde <sahil.shinde@trunexa.com>
2024-02-14 11:43:45 +0530

    configs/imx8mm_trucrux_defconfig:Enable key features

---
+++ imx8mm_trucrux_config/include/generated/timestamp_autogenerated.h
@@ -0,0 +1,5 @@
+#define U_BOOT_DATE "Feb 07 2024"
+#define U_BOOT_TIME "11:58:07"
+#define U_BOOT_TZ "+0000"
+#define U_BOOT_DMI_DATE "02/07/2024"
+#define U_BOOT_BUILD_DATE 0x20240207
---
+++ imx8mm_trucrux_config/include/generated/version_autogenerated.h
@@ -0,0 +1,4 @@
+#define PLAIN_VERSION "2020.04-v2020.04_5.4.24_2.1.0_dunfell+ge5b946d526"
+#define U_BOOT_VERSION "U-Boot " PLAIN_VERSION
+#define CC_VERSION_STRING "aarch64-fslc-linux-gcc (GCC) 9.3.0"
+#define LD_VERSION_STRING "GNU ld (GNU Binutils) 2.34.0.20200220"
